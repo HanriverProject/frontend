9b684b0a48decbe795f18093c5d84d9407dfc4b1
22yuu <22yuu@naver.com>
2023-05-24 23:35:27 +0900

    feat: Header 컴포넌트 css 입히기 및 중복되는 코드 로직 분리

--- client/src/components/layouts/Header.tsx
+++ client/src/components/layouts/Header.tsx
@@ -10,55 +10,91 @@ import notify from '@/assets/icons/notify.svg';
 
 interface HeaderProps {}
 
+const categorys = ['국내도서', '해외도서', 'eBook'];
+const headerTopRight = ['회원가입', '로그인'];
+
+type HeaderListItemProps = {
+  text: string;
+};
+
+const HeaderListItem = ({ text }: HeaderListItemProps) => {
+  return (
+    <li>
+      <Link href="#">
+        <span className="text-2xl text-slate-lightgrey">{text}</span>
+      </Link>
+    </li>
+  );
+};
+
 const Header: FC<HeaderProps> = ({}) => {
+  const logoDropShadow = {
+    filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+  };
   return (
     <header className="flex flex-col w-full">
       <section
         id="header-top"
-        className="flex items-center justify-between w-full h-20 py-2 px-16 border-b border-b-gray-400"
+        className="flex items-center justify-between w-full h-20 py-2 px-16 border-b border-b-slate-lightgrey"
       >
         <div id="header-top-left">
-          <ul className="flex gap-3">
-            <li>
-              <Link href="#">국내도서</Link>
-            </li>
-            <li>
-              <Link href="#">해외도서</Link>
-            </li>
-            <li>
-              <Link href="#">eBook</Link>
-            </li>
+          <ul className="flex gap-8">
+            {categorys.map((category, index) => (
+              <HeaderListItem key={index} text={category} />
+            ))}
           </ul>
         </div>
 
         <div id="header-top-right">
-          <ul className="flex gap-3">
-            <li>
-              <Link href="#">회원가입</Link>
-            </li>
-            <li>
-              <Link href="#">로그인</Link>
-            </li>
+          <ul className="flex gap-8">
+            {headerTopRight.map((text, index) => (
+              <li key={index}>
+                <Link href="#">
+                  <span className="text-2xl text-slate-lightgrey">{text}</span>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </section>
       <section
         id="header-bottom"
-        className="flex items-center w-full h-20 py-2 px-20 justify-between"
+        className="flex items-center w-full py-8 px-20 justify-between"
       >
-        <div id="header-bottom-left" className="flex items-center gap-2">
-          <Image src={logo} width={64} height={64} alt="Logo Image" />
-          <h2>HANRIVER BOOKS</h2>
-        </div>
-        <div id="header-bottom-right" className="flex itmes-center gap-8">
-          <div id="header-searchbar">
+        <Link href="/" as={'image'}>
+          <div id="header-bottom-left" className="flex items-center gap-2">
+            <Image
+              src={logo}
+              width={64}
+              height={64}
+              style={logoDropShadow}
+              alt="Logo Image"
+            />
+            <div className="font-bold text-4xl">
+              <span className="text-accent-blue">HAN</span>
+              RIVER BOOKS
+            </div>
+          </div>
+        </Link>
+        <div
+          id="header-bottom-right"
+          className="flex itmes-center justify-between gap-8 w-3/10 "
+        >
+          <div
+            id="header-searchbar"
+            className="flex items-center p-2 border rounded-2xl border-gray-100 bg-gray-100"
+          >
             <button>
               <Image src={search} width={36} height={36} alt="Search Icon" />
             </button>
-            <input type="text" placeholder="Search something..." />
+            <input
+              type="text"
+              placeholder="Search something..."
+              className="w-80 ml-2 bg-transparent"
+            />
           </div>
-          <div>
-            <ul className="flex justify-evenly">
+          <div className="w-full">
+            <ul className="flex w-full gap-16">
               <li>
                 <button>
                   <Image
@@ -68,9 +104,13 @@ const Header: FC<HeaderProps> = ({}) => {
                     alt="Notify Icon"
                   />
                 </button>
+              </li>
+              <li>
                 <button>
                   <Image src={cart} width={48} height={48} alt="Cart Icon" />
                 </button>
+              </li>
+              <li>
                 <button>
                   <Image
                     src={mypage}
