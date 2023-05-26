d4a04d7642ba41da2a0370c7934b16f0acee3192
xion <calf-love@naver.com>
2023-05-26 14:43:22 +0900

    delete unuse icon

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Button from '@/components/ui/Button';
-import MoonIcon from '@/assets/icons/MoonIcon';
 import DarkmodeToggleBtn from '@/components/DarkmodeToggleBtn';
 
 export default function Home() {
@@ -8,9 +7,7 @@ export default function Home() {
       <Button variant={'grey'} size={'lg'} label={'Try Again'}></Button>
       <Button variant={'grey'} size={'md'} label={'장바구니'}></Button>
       <Button variant={'blue'} size={'wlg'} label={'회원가입'}></Button>
-      <Button>
-        <MoonIcon />
-      </Button>
+
       <DarkmodeToggleBtn />
     </main>
   );
