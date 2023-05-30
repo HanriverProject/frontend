d10489d4f33c5160dff6e5456f684fb9e97e0ffc
xion <calf-love@naver.com>
2023-05-30 16:42:08 +0900

    [CREATE] google oauth but little problem

--- client/src/components/layouts/Providers.tsx
+++ client/src/components/layouts/Providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { FC } from 'react';
+import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 
 interface ProvidersProps {
@@ -8,11 +9,13 @@ interface ProvidersProps {
 
 const Providers: FC<ProvidersProps> = ({ children }: ProvidersProps) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <div className="flex flex-col min-h-screen select-none text-gray-700 transition-colors duration-300 dark:bg-slate-deepdark dark:text-gray-200">
-        {children}
-      </div>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <div className="flex flex-col min-h-screen select-none text-gray-700 transition-colors duration-300 dark:bg-slate-deepdark dark:text-gray-200">
+          {children}
+        </div>
+      </ThemeProvider>
+    </SessionProvider>
   );
 };
 
